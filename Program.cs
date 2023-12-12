ebb1c81263a71ab3ad5c3c9bdf219a942a101f6b
Igor Zywicki <igor120087@gmail.com>
2023-12-12 10:18:42 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class NamesScoreCalculator
+{
+    private string[] names;
+
+    public NamesScoreCalculator()
+    {
+        names = File.ReadAllText("names.txt").Split(",").OrderBy(name => name).ToArray();
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            names[i] = names[i].Trim('"');
+        }
+
+    }
+
+    public void CalculateTotalScore()
+    {
+        long total = 0;
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            char[] name = names[i].ToCharArray();
+
+            total += CalculateNameScore(name, i);
+
+        }
+
+        Console.WriteLine(total);
+    }
+
+    public long CalculateNameScore(char[] name, int position)
+    {
+        long score = 0;
+        for (int i = 0; i < name.Length; i++)
+        {
+            score += GetLetterValue(name[i]);
+        }
+        score *= position + 1;
+        return score;
+    }
+
+    public int GetLetterValue(char letter)
+    {
+        int value = char.ToUpper(letter) - 64;
+
+        return value;
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NamesScoreCalculator nameCalculator = new NamesScoreCalculator();
+        nameCalculator.CalculateTotalScore();
+    }
+}
+
+
